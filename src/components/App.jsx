e2cf0e360826d6e22ecef407f83046186f850691
Julia Maykut <pankiulia@gmail.com>
2023-05-05 21:12:55 +0300

    add contact list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+import { data } from '../data/user';
+import Button from './Button/Button';
+import { UserList } from './UsersList/UserList';
+export class App extends Component {
+  state = {
+    users: data,
+    isListShows: false,
+  };
+  showList = () => {
+    this.setState({ isListShows: true });
+  };
+  deleteUser = id => {
+    this.setState(prevState => {
+      return {
+        users: prevState.users.filter(user => user.id !== id),
+      };
+    });
+  };
+  render() {
+    const { users, isListShows } = this.state;
+    return (
+      <div>
+        {!isListShows ? (
+          <Button text="Show list of user" clickHandler={this.showList} />
+        ) : (
+          <UserList users={users} deleteUser={this.deleteUser} />
+        )}
+      </div>
+    );
+  }
+}
---
+++ src/components/UsersList/UserList.js
@@ -0,0 +1,13 @@
+export const UserList = ({ users, deleteUser }) => (
+  <ul>
+    {users.map(({ name, email, id }) => (
+      <li key={id}>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <button type="button" onClick={() => deleteUser(id)}>
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
